3a797bc19a62ee3be17ead9020ec2e9d0a74549a
Xandeon <xandeonflux@gmail.com>
2024-09-05 18:56:26 -0600

    First Commit, Liquid Drop Model Script

---
+++ src/main.rs
@@ -0,0 +1,126 @@
+
+/*
+ * Benjamin Waters 
+ * Nuclear Physics
+ * Liquid Drop Model Binding Energy Calculato
+ *
+ * 9/5/2024
+ *
+*/
+
+#![allow(non_upper_case_globals)]   // for scientific units
+#![allow(non_snake_case)]           // for element names such as U for Uranium
+
+// ---------------------------- units ----------------------------------
+
+const eV: f64 = 1.0;
+const keV: f64 = 1000.0;
+const MeV: f64 = 1000.0 * 1000.0;
+const GeV: f64 = 1000.0 * 1000.0 * 1000.0;
+
+// ---------------------------- liquid drop Model constants ------------
+
+const A_V: f64 = 15.5; // MeV (volume coefficient)
+const A_S: f64 = 16.8; // MeV (surface area coeffiecient)
+const A_C: f64 = 0.72; // eV (coulomb coefficient)
+const A_A: f64 = 23.0; // MeV (asymmetry term coefficient)
+const A_P: f64 = 34.0; // MeV (pairing term coefficient)                       
+
+fn print_constants(){
+    println!("a_v: {:.2} MeV", A_V);
+    println!("a_s: {:.2} MeV", A_S);
+    println!("a_c: {:.2} MeV", A_C);
+    println!("a_A: {:.2} MeV", A_A);
+    println!("a_p: {:.2} MeV", A_P);
+}
+
+struct Isotope{
+    element: String,
+    A: i32,
+    Z: i32,
+    N: i32,
+}
+
+impl Isotope{
+    fn create_isotope(element: String, A: i32, Z: i32, N: i32) -> Self{
+        Self{
+            element, A, Z, N,
+        }
+    }
+
+    fn binding_energy(&self) -> f64{
+        /// Liquid Drop Model
+        /// Implements the Bethe-Weinsaecker semi-empirical mass formula for binding energy
+        ///
+        /// The calculation is performed in units of MeV and can be converted afterward to 
+        /// other units to not clutter the calculation. Only the coloumb term is scaled.
+        
+        let Isotope {A, Z, N, ..} = self; 
+        let A_f64: f64 = *A as f64;
+        let Z_f64: f64 = *Z as f64;
+        let N_f64: f64 = *N as f64;
+
+        let volume = A_V * A_f64;
+        let surface = -1.0 * A_S * A_f64.powf(2.0 / 3.0);
+        let coulomb = -1.0 * A_C * Z_f64.powf(2.0) * A_f64.powf(-1.0 / 3.0); 
+        let asymetry = -1.0 * A_A * (N_f64 - Z_f64).powf(2.0)/A_f64;
+        let delta = A_P * A_f64.powf(-3.0 / 4.0);
+
+        let pairing ={
+            if Z % 2 == 0 && N % 2 == 0{            // Even Z / Even N
+                delta
+            } else if Z % 2 != 0 && N % 2 == 0{     // Odd Z / Even N
+                0.0
+            } else if Z % 2 != 0 && N % 2 != 0{     // Odd Z / Odd N
+                -1.0 * delta
+            } else {                                // Not a common case
+                0.0    
+            } 
+        };
+    
+        volume + surface + coulomb + asymetry + pairing // return the binding energy
+    }
+
+    fn binding_energy_per_nucleon(&self) -> f64{
+        // Returns the binding energy per nucleon of the Isotope
+    
+        self.binding_energy() / self.A as f64     
+    }
+
+    fn report(&self) {
+        println!(
+                "------------- Isotope Report ------------ \n\
+                 {:<20} {:>15} \n\
+                 {:<20} {:>15} nucleons \n\
+                 {:<20} {:>15} protons \n\
+                 {:<20} {:>15} neutrons \n\
+                 {:<20} {:>15.3} MeV \n\
+                 {:<20} {:>15.3} MeV \n\
+                 -----------------------------------------", 
+                 "Element:", self.element,
+                 "A", self.A,
+                 "Z", self.Z,
+                 "N", self.N,
+                 "Binding Energy:", self.binding_energy(),
+                 "BE/A:", self.binding_energy_per_nucleon()
+                 );
+    }
+}
+
+
+fn main() {
+    
+    let U_236 = Isotope::create_isotope(String::from("Uranium"), 236, 92, 144);
+
+    let Pd_117 = Isotope::create_isotope(String::from("Palladium"), 117, 46, 117 - 4);
+
+    let Xe_140 = Isotope::create_isotope(String::from("Xenon"), 140, 54, 140 - 54);
+
+    let Sr_94 = Isotope::create_isotope(String::from("Strontium"), 94, 38, 94 - 38);
+    
+
+    U_236.report();
+    Pd_117.report();
+    Xe_140.report();
+    Sr_94.report();    
+}
